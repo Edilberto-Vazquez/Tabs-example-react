59a46bb2f9a99df78c1269e06f251775ed1eee18
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-06-25 21:25:59 -0500

    "add a children"

--- src/components/CreateTabs.jsx
+++ src/components/CreateTabs.jsx
@@ -1,39 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const Tabs = ({children}) => {
-    console.log(children)
-    const [activeTab, setActiveTab] = useState(children[0].props.label)
-    
-    return (
-        <div>
-        <div className="tab-list">
-        { children.map(item => {
-                const {label} = item.props
-                let className = 'tab-list-item';
-                if (activeTab === label) {
-                  className += ' tab-list-active';
-                }
-                return (
-                    <div className={className} onClick={() => setActiveTab(label)}>
-                        <>{label}</>
-                    </div>
-                )
-            })}
-        </div>
-        <div className="tab-content">
-          {children.map((item) => {
-            if (item.props.label !== activeTab) return undefined;
-            return item.props.children;
-          })}
-        </div>
-        </div>
-    )
-}
+export const Tabs = ({ children }) => {
+  console.log(children);
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
 
-export const Tab = ({label}) => {
-    return (
-        <>
-            {label}
-        </>
-    )
-}
+  return (
+    <div>
+      <div className="tab-list">
+        {children.map((item) => {
+          const { label } = item.props;
+          let className = "tab-list-item";
+          if (activeTab === label) {
+            className += " tab-list-active";
+          }
+          return (
+            <div className={className} onClick={() => setActiveTab(label)}>
+              <>{label}</>
+            </div>
+          );
+        })}
+      </div>
+      <div className="tab-content">
+        {children.map((item) => {
+          if (item.props.label !== activeTab) return undefined;
+          return item.props.children;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export const Tab = ({ label }) => {
+  return <>{label}</>;
+};
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import {Tabs, Tab} from './CreateTabs'
+import React from "react";
+import { Tabs, Tab } from "./CreateTabs";
 
 const Items = () => {
-    return (
-        <div>
-            <h1>Tabs</h1>
-            <Tabs>
-                <Tab label="one"/>
-                <Tab label="two"/>
-                <Tab label="three"/>
-            </Tabs>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est in voluptatibus commodi iusto, a, dolores obcaecati aliquam quos repudiandae vel dolorem mollitia laudantium numquam fugiat sapiente cum voluptatum doloremque repellendus.</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Tabs</h1>
+      <Tabs>
+        <Tab label="one">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          inventore similique aliquid odio at cupiditate est, ad ipsum, pariatur
+          vero tempora vitae? Inventore magnam sint, recusandae libero aliquam
+          repellendus quos!
+        </Tab>
+        <Tab label="two">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos qui
+          voluptatem cupiditate maiores numquam itaque voluptate sunt explicabo.
+          Autem quam ut reiciendis cum modi, doloribus aperiam eaque id numquam
+          sapiente.
+        </Tab>
+        <Tab label="three">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          in, accusantium deleniti, itaque molestias, incidunt ex molestiae
+          praesentium distinctio hic non porro nulla! Ea totam nostrum ad
+          voluptas distinctio. Illo.
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
 
-export default Items
+export default Items;
